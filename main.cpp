012c9620acae93f86bf9b88c3e633b02c4cc5a0a
Alec Jacobson <alecjacobson@gmail.com>
2021-10-08 09:54:43 -0400

    just extract indices

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include <igl/doublearea.h>
 #include <igl/copyleft/cgal/remesh_self_intersections.h>
 #include <igl/slice_mask.h>
+#include <igl/unique.h>
+#include <igl/slice.h>
+#include <igl/find.h>
 #include <igl/unique_edge_map.h>
 #include <igl/remove_unreferenced.h>
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char * argv[])
 {
@@ -16,30 +20,26 @@ int main(int argc, char * argv[])
   Eigen::MatrixXd A;
   igl::doublearea(V,F0,A);
   Eigen::MatrixXi F;
-  igl::slice_mask(F0,(A.array()>1e-16).eval(),1,F);
+  Eigen::VectorXi J;
+  igl::find((A.array()>1e-16).eval(),J);
+  igl::slice(F0,J,1,F);
+  // Compute which faces intersect
   igl::copyleft::cgal::RemeshSelfIntersectionsParam params;
-  params.detect_only = false;
+  params.detect_only = true;
   params.first_only = false;
   params.stitch_all = true;
-  Eigen::MatrixXd W;
-  Eigen::MatrixXi G;
-  Eigen::MatrixXi IF;
-  Eigen::VectorXi J,IM;
-  igl::copyleft::cgal::remesh_self_intersections(V,F,params,W,G,IF,J,IM);
-  Eigen::MatrixXi uE;
-  Eigen::VectorXi EMAP,uEC,uEE;
+  Eigen::VectorXi I;
   {
-    Eigen::MatrixXi _;
-    igl::unique_edge_map(G,_,uE,EMAP,uEC,uEE);
+    Eigen::MatrixXd W;
+    Eigen::MatrixXi G;
+    Eigen::MatrixXi IF;
+    Eigen::VectorXi K,IM;
+    igl::copyleft::cgal::remesh_self_intersections(V,F,params,W,G,IF,K,IM);
+    // Unique list of face ids
+    Eigen::VectorXi U;
+    igl::unique(IF,U);
+    // Remap indices to mesh before removing degenerates
+    igl::slice(J,U,1,I);
   }
-  Eigen::MatrixXi E;
-  igl::slice_mask(uE,(uEC.tail(uE.rows())-uEC.head(uE.rows())).array()>2,1,E);
-  {
-    Eigen::VectorXi _;
-    igl::remove_unreferenced(Eigen::MatrixXd(W),Eigen::MatrixXi(E),W,E,_);
-  }
-  std::ofstream s(argv[2]);
-  s<<
-    W.format(Eigen::IOFormat(Eigen::FullPrecision,Eigen::DontAlignCols," ","\n","v ","","","\n"))<<
-    (E.array()+1).format(Eigen::IOFormat(Eigen::FullPrecision,Eigen::DontAlignCols," ","\n","l ","","","\n"));
+  std::cout<<I.transpose()<<std::endl;
 }
